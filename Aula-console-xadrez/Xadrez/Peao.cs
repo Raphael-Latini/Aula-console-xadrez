1012eae152c4b567fe1660ad4e1126c70f23d7c8
Raphael Latini <raphael.latini@gmail.com>
2022-03-15 20:58:51 -0300

    Implementando jogadas especiais

--- Aula-console-xadrez/Xadrez/Peao.cs
+++ Aula-console-xadrez/Xadrez/Peao.cs
@@ -49,32 +49,36 @@ namespace Xadrez
                 {
                     mat[pos.linha, pos.coluna] = true;
                 }
-                else
+            }
+            else
+            {
+                pos.definirValores(posicao.linha + 1, posicao.coluna);
+                if (tab.posicaoValida(pos) && livre(pos))
                 {
-                    pos.definirValores(posicao.linha + 1, posicao.coluna);
-                    if (tab.posicaoValida(pos) && livre(pos))
-                    {
-                        mat[pos.linha, pos.coluna] = true;
-                    }
-                    pos.definirValores(posicao.linha + 2, posicao.coluna);
-                    if (tab.posicaoValida(pos) && livre(pos) && quantidadeMovimento == 0)
-                    {
-                        mat[pos.linha, pos.coluna] = true;
-                    }
-                    pos.definirValores(posicao.linha + 1, posicao.coluna - 1);
-                    if (tab.posicaoValida(pos) && existeInimigo(pos))
-                    {
-                        mat[pos.linha, pos.coluna] = true;
-                    }
-                    pos.definirValores(posicao.linha + 1, posicao.coluna + 1);
-                    if (tab.posicaoValida(pos) && existeInimigo(pos))
-                    {
-                        mat[pos.linha, pos.coluna] = true;
-                    }
-
+                    mat[pos.linha, pos.coluna] = true;
                 }
+                pos.definirValores(posicao.linha + 2, posicao.coluna);
+                if (tab.posicaoValida(pos) && livre(pos) && quantidadeMovimento == 0)
+                {
+                    mat[pos.linha, pos.coluna] = true;
+                }
+                pos.definirValores(posicao.linha + 1, posicao.coluna - 1);
+                if (tab.posicaoValida(pos) && existeInimigo(pos))
+                {
+                    mat[pos.linha, pos.coluna] = true;
+                }
+                pos.definirValores(posicao.linha + 1, posicao.coluna + 1);
+                if (tab.posicaoValida(pos) && existeInimigo(pos))
+                {
+                    mat[pos.linha, pos.coluna] = true;
+                }
+
             }
             return mat;
         }
+
     }
-}
+    
+ }
+    
+
--- Aula-console-xadrez/Xadrez/Rei.cs
+++ Aula-console-xadrez/Xadrez/Rei.cs
@@ -5,8 +5,10 @@ namespace Xadrez
 {
     class Rei : Peca
     {
-        public Rei(Tabuleiro tab, Cor cor): base(tab, cor)
+        private PartidaDeXadrez partida;
+        public Rei(Tabuleiro tab, Cor cor,PartidaDeXadrez partida): base(tab, cor)
         {
+            this.partida = partida;
         }
         public override string ToString()
         {
@@ -16,6 +18,11 @@ namespace Xadrez
         {
             Peca p = tab.peca(pos);
             return p == null || p.cor != cor; 
+        }
+        private  bool testeTorreParaRoque(Posicao pos)
+        {
+            Peca p = tab.peca(pos);
+            return p != null && p is Torre && p.cor == cor && p.quantidadeMovimento == 0;
         }
         public override bool[,] movimentosPossiveis()
         {
@@ -71,12 +78,33 @@ namespace Xadrez
             {
                 mat[pos.linha, pos.coluna] = true;
             }
+            // #jogadaespecial roque 
+            if(quantidadeMovimento==0 && !partida.xeque)
+            {       //roque pequeno
+                Posicao posTorre1 = new Posicao(posicao.linha, posicao.coluna + 3);
+                if (testeTorreParaRoque(posTorre1))
+                {
+                    Posicao p1 = new Posicao(posicao.linha, posicao.coluna+1);
+                    Posicao p2 = new Posicao(posicao.linha, posicao.coluna + 2);
+                    if(tab.peca(p1)==null && tab.peca(p2) == null)
+                    {
+                        mat[posicao.linha, posicao.coluna + 2] = true;
+                    }
+                }
+                //roque Grande
+                Posicao posTorre2 = new Posicao(posicao.linha, posicao.coluna -4);
+                if (testeTorreParaRoque(posTorre2))
+                {
+                    Posicao p1 = new Posicao(posicao.linha, posicao.coluna - 1);
+                    Posicao p2 = new Posicao(posicao.linha, posicao.coluna - 2);
+                    Posicao p3 = new Posicao(posicao.linha, posicao.coluna - 3);
+                    if (tab.peca(p1) == null && tab.peca(p2) == null && tab.peca(p3)==null)
+                    {
+                        mat[posicao.linha, posicao.coluna + 2] = true;
+                    }
+                }
+            }
             return mat;
-
-
-
-
-
         }
     }
   
